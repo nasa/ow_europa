5ed6a9cfb7b83d15d7bccdb5e84e74384ac18a74
Ussama Naal <ussama.naal@nasa.gov>
2020-07-29 10:20:13 -0700

    Include the recommended y1a dataset + Add model resources path to gazebo's resource path + rename model name

--- env-hooks/70.ow_europa.sh
+++ env-hooks/70.ow_europa.sh
@@ -1,2 +1,2 @@
 export GAZEBO_MODEL_PATH="$(catkin_find --first-only ow_europa models):${GAZEBO_MODEL_PATH}"
-
+export GAZEBO_RESOURCE_PATH="$(catkin_find --first-only ow_europa models):${GAZEBO_RESOURCE_PATH}"
